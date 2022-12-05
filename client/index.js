5367073e7e2656625acec2f370d91baadb466f5b
devilbats011 <wanafnanharizwz@gmail.com>
2022-12-05 08:15:55 +0800

    update-ticket

--- client/index.js
+++ client/index.js
@@ -24,34 +24,59 @@ const TOTAL_TICKETS = 10;
 const EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 const accountEL = document.getElementById("account");
+
 const ticketsEl = document.getElementById("tickets");
 
-let main = async () => {
-  const accs = await web3.eth.requestAccounts();
-  console.log(accs);
-  let account0 = accs[0];
-  accountEL.innerText = account0;
+let acc = undefined;
+
+const buyTicket = async (ticket) => {
+  await contract.methods.buyTicket(ticket.id).send({
+    from: acc,
+    value: ticket.price,
+  });
+  await refreshTickets();
+};
+
+const refreshTickets = async () => {
+  ticketsEl.innerHTML = '';
   for (let i = 0; i < TOTAL_TICKETS; i++) {
+    /**
+     * ? need to learn more about call function @@..
+     * ? why need call...?
+     * @answer * .call return an object instead of tickets(i) function, in this case, we need object not function
+     * console.log(await contract.methods.tickets(i), ticket) <- the first arguement/paramater will output function , the second arguement/paramater will output object
+     **/
     const ticket = await contract.methods.tickets(i).call();
     ticket.id = i;
     if (ticket.owner === EMPTY_ADDRESS) {
       const ticketEl = createElementFromString(
         `<div class="ticket card" style="width: 18rem;">
-                  <img src="${ticketImage}" class="card-img-top" alt="...">
-                  <div class="card-body">
-                    <h5 class="card-title">Ticket</h5>
-                    <p class="card-text">${ticket.price / 1e18} Eth</p>
-                    <button class="btn btn-primary">Buy Ticket</button>
-                  </div>
-                </div>`
+                      <img src="${ticketImage}" class="card-img-top" alt="ticket">
+                      <div class="card-body">
+                        <h5 class="card-title">Ticket</h5>
+                        <p class="card-text">${ticket.price / 1e18} Eth</p>
+                        <button class="btn btn-primary">Buy Ticket</button>
+                      </div>
+                    </div>`
       );
+
       /**
-       * ? need to learn more about bind and call functions @@..
-      **/
-      //   ticketEl.onclick = buyTicket.bind(null, ticket);
+       * ? need to learn more about bind function @@..
+       * ? why and when need bind...?
+       * @answer * .bind - the buyTicket method is bind to the onclick event, it does not execute immediately , It only execute when onclick event triggered.
+       * ticketEl.onclick = buyTicket(ticket); <- this syntax will execute immdiately which is bad case for onclick event
+       **/
+      ticketEl.onclick = buyTicket.bind(null, ticket);
       ticketsEl.appendChild(ticketEl);
     }
   }
+};
+
+const main = async () => {
+  const accs = await web3.eth.requestAccounts();
+  acc = accs[0];
+  accountEL.innerText = acc;
+  await refreshTickets();
 };
 
 main();
